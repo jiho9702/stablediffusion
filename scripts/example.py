68e27edcc7c01d775459a85bf8bb4b7eb74ce82f
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-03-25 16:21:05 +0900

    change

---
+++ scripts/example.py
@@ -0,0 +1,1 @@
+command = "python scripts/txt2img.py --prompt An astronaunt riding a horse --ckpt scripts/v2-1_512-ema-pruned.ckpt --scale 5.0 --config configs/stable-diffusion/v2-inference.yaml --W 704 --H 512"
